7ab1f9a9e3f6278589ecdbb46fc198424f5a8fa8
Travis Hein <marsairforce@gmail.com>
2018-03-30 09:12:47 -0400

    add support for uint8_t

--- bit_macros.h
+++ bit_macros.h
@@ -5,8 +5,13 @@
 #ifndef _bit_macros_
 #define _bit_macros_
 
+#include <inttypes.h>
 
 static inline void set_bit(unsigned int *x, unsigned int mask) {
+  *x |= mask;
+}
+
+static inline void set_bit(uint8_t *x, uint8_t mask) {
   *x |= mask;
 }
 
@@ -14,7 +19,15 @@ static inline void clear_bit(unsigned int *x, unsigned int mask) {
   *x &= ~mask;
 }
 
+static inline void clear_bit(uint8_t *x, uint8_t mask) {
+  *x &= ~mask;
+}
+
 static inline void toggle_bit(unsigned int *x, unsigned int mask) {
+  *x ^= mask;
+}
+
+static inline void toggle_bit(uint8_t *x, uint8_t mask) {
   *x ^= mask;
 }
 
@@ -22,4 +35,7 @@ static inline int test_bit(unsigned int *x, unsigned int mask) {
   return (*x & mask);
 }
 
+static inline int test_bit(uint8_t *x, uint8_t mask) {
+  return (*x & mask);
+}
 #endif
